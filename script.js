cbd7dffebe1df05d9106df00337fd799b8d36508
Tranzzid <96055621+Tranzzid@users.noreply.github.com>
2022-11-17 13:34:55 +0100

    klart v 46, search fungerar med dynamisk lista

--- script.js
+++ script.js
@@ -4,26 +4,30 @@ const booklist = [
     {
         id: 1,
         author: "Charles Dickens",
-        title: "Oliver Teist"
+        title: "Oliver Twist"
     },
     
     {
-        id: 1,
-        author: "Charles Dickens",
-        title: "Oliver Teist"
+        id: 2,
+        author: "Hamilton",
+        title: "Kalle-Anka"
     }
-];
+]
+
 const searchInput = null;
 
-function handleKeyPress(input) {
+const searchField = document.getElementById("searchField");
+searchField.addEventListener("keyup", handleKeyPress);
+
+
+function handleKeyPress(event) {
     /*
     Ta emot/läsa av värdet i inputfältet.
     Sicka värdet till searchBooks.
     searchBooks returnerar en filtrerad liseta.
     Filtrerade lisstan skickas till renderBookList
      */
-
-    searchBooks(input);
+    searchBooks(event.target.value);
 }
 
 function searchBooks(searchTerm) {
@@ -36,19 +40,38 @@ function searchBooks(searchTerm) {
     
     */
     const filteredList = [];
-    for (let i = 0; i < booklist.length; i++) {
-        const title = booklist[i].title.toLowerCase();
-        if (title.indexOf(searchTerm.toLowerCase()) >= 0) {
-            filteredList.push(booklist[0]);
+    if (searchTerm.length > 0) {
+        for (let i = 0; i < booklist.length; i++) {
+            const title = booklist[i].title.toLowerCase();
+            if (title.indexOf(searchTerm.toLowerCase()) >= 0) {
+                filteredList.push(booklist[i]);
+            }
         }
     }
+    
     renderBookList(filteredList)
 }
 
-function renderBookList(list) {
+function renderBookList(bookList) {
     /* Element i HTML-listan visas/döljs beroende på listans innehåll. */
-    console.log(list);
+    const existingElement = document.querySelector(".book-list");
+    if (existingElement) {
+        root.removeChild(existingElement);
+    }
+    if (bookList.length > 0) {
+        let html = `<ul class = "book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`
+    
+                for(let i = 0; i < bookList.length; i++) {
+                    html += `<li 
+                                class = "book-list_item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+                                ${bookList[i].author} - ${bookList[i].title}
+                            </li>`;
+                }
 
+            html += `</ul>`;
+
+        const root = document.getElementById("root");
+
+        root.insertAdjacentHTML("beforeend", html);
+    }
 }
-
-handleKeyPress("e")
